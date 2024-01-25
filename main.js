a152b2a9365a11fa1df7d7bae71cb241d884476d
szilvia-barath <157715907+szilvia-barath@users.noreply.github.com>
2024-01-25 16:36:39 +0100

    files

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var transparencyLink = document.getElementById('transparency-link');
+    var cardsContainer = document.getElementById('cards-container');
+    var logoRegular = document.querySelector('.logo-regular');
+    var textInfo = document.querySelector('.text-info');
+    var bgGradient = document.querySelector('.bg-gradient');
+    var transparencyText = document.querySelector('.transparency-text');
+    var backButton = document.querySelector('.btn-primary');
+
+    transparencyLink.addEventListener('click', function () {
+        cardsContainer.style.display = (cardsContainer.style.display === 'none') ? 'flex' : 'none';
+        textInfo.style.display = (textInfo.style.display === 'none') ? 'block' : 'none';
+        logoRegular.classList.toggle('smaller-logo');
+        bgGradient.classList.toggle('larger-bg-gradient');
+        transparencyText.classList.remove('hidden');
+        transparencyText.classList.add('dynamic-text');
+    });
+
+    backButton.addEventListener('click', function () {
+        cardsContainer.style.display = 'flex';
+        textInfo.style.display = 'block';
+        logoRegular.classList.remove('smaller-logo');
+        bgGradient.classList.remove('larger-bg-gradient');
+        transparencyText.classList.remove('dynamic-text');
+        transparencyText.classList.add('hidden');
+    });
+});
